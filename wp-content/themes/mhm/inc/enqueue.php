ac4f6b761d00d98af569364252311b1ee9d586f6
Antonio Molinari <molinari@incode.it>
2016-05-19 03:09:30 +0200

    working off-canvas menu

--- wp-content/themes/mhm/inc/enqueue.php
+++ wp-content/themes/mhm/inc/enqueue.php
@@ -6,14 +6,11 @@
  */
 
 function understrap_scripts() {
-<<<<<<< HEAD
 
     /* codyhouse style and scripts */
     wp_enqueue_script( 'codyhouse-menu-scripts', get_template_directory_uri() . '/js/codyhouse_menu.js', array(), '0.3.8', true );
     wp_enqueue_style( 'codyhouse-menu-styles', get_stylesheet_directory_uri() . '/css/codyhouse_menu.css', array(), '0.3.8');
 
-=======
->>>>>>> 0ac20354be6562986183665bce39f7b8a2950bc7
     //wp_enqueue_style( 'understrap-styles', get_stylesheet_directory_uri() . '/css/theme.min.css', array(), '0.3.8');
     wp_enqueue_style( 'understrap-styles', get_stylesheet_directory_uri() . '/css/theme.css', array(), '0.3.8');
     wp_enqueue_script('jquery');
@@ -23,12 +20,9 @@ function understrap_scripts() {
         wp_enqueue_script( 'comment-reply' );
     }
 
-<<<<<<< HEAD
-=======
     /* codyhouse style and scripts */
     wp_enqueue_script( 'codyhouse-menu-scripts', get_template_directory_uri() . '/js/codyhouse_menu.js', array(), '0.3.8', true );
     wp_enqueue_style( 'codyhouse-menu-styles', get_stylesheet_directory_uri() . '/css/codyhouse_menu.css', array(), '0.3.8');
->>>>>>> 0ac20354be6562986183665bce39f7b8a2950bc7
 
 }
 
--- wp-content/themes/mhm/js/codyhouse_menu.js
+++ wp-content/themes/mhm/js/codyhouse_menu.js
@@ -15,7 +15,6 @@ jQuery(document).ready(function($){
 		var target = $(this),
 			//detect which section user has chosen
 			sectionTarget = target.data('menu');
-<<<<<<< HEAD
 
 		/* modified by biennio */
 		var sectionUrl = target.find("a").attr("href")
@@ -24,18 +23,12 @@ jQuery(document).ready(function($){
 		/* end modified by biennio */
 
 
-=======
->>>>>>> 0ac20354be6562986183665bce39f7b8a2950bc7
 		if( !target.hasClass('cd-selected') ) {
 			//if user has selected a section different from the one alredy visible
 			//update the navigation -> assign the .cd-selected class to the selected item
 			target.addClass('cd-selected').siblings('.cd-selected').removeClass('cd-selected');
 			//load the new section
-<<<<<<< HEAD
 			loadNewContent(sectionName, sectionUrl);
-=======
-			loadNewContent(sectionTarget);
->>>>>>> 0ac20354be6562986183665bce39f7b8a2950bc7
 		} else {
 			// otherwise close navigation
 			toggleNav(false);
@@ -47,7 +40,6 @@ jQuery(document).ready(function($){
 		$('main').toggleClass('scale-down', bool);
 	}
 
-<<<<<<< HEAD
 	function loadNewContent(sectionName, sectionUrl) {
 		//alert("sectionUrl: "+sectionUrl+", sectionName: "+sectionName)
 		//create a new section element and insert it into the DOM
@@ -55,13 +47,6 @@ jQuery(document).ready(function($){
 		//load the new content from the proper html file
 		//section.load(sectionName+'.html .cd-section > *', function(event){
 		section.load(sectionUrl+" .cd-section > *", function(event){
-=======
-	function loadNewContent(newSection) {
-		//create a new section element and insert it into the DOM
-		var section = $('<section class="cd-section '+newSection+'"></section>').appendTo($('main'));
-		//load the new content from the proper html file
-		section.load(newSection+'.html .cd-section > *', function(event){
->>>>>>> 0ac20354be6562986183665bce39f7b8a2950bc7
 			//add the .cd-selected to the new section element -> it will cover the old one
 			section.addClass('cd-selected').on('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
 				//close navigation
@@ -81,8 +66,4 @@ jQuery(document).ready(function($){
 			section.prev('.cd-section').remove();
 		}
 	}
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> 0ac20354be6562986183665bce39f7b8a2950bc7
